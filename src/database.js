a43ff7afc7f1838d051b407b8707b3b0ff927aa7
jgretz <jgretz@truefitsolutions.com>
2016-12-28 16:11:49 -0500

    expose calls to hooks

--- src/database.js
+++ src/database.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
-import { mapFuncType, mapComplexType } from './util';
+import {
+  mapFuncType, mapComplexType,
+  QUERY, INSERT, UPDATE, DELETE,
+  BEFORE, AFTER,
+} from './util';
 
 // helpers
 const mapField = (value) => {
@@ -40,6 +44,8 @@ export default class Sql {
 
   // schema management
   synchronizeSchema(schema) {
+    this.definedSchema = schema;
+
     const keys = _.keys(schema);
 
     _.forEach(keys, (key) => {
@@ -67,23 +73,67 @@ export default class Sql {
   }
 
   // crud
+  execute(name, action, args, logic) {
+    const hooks = this.config.hooks || [];
+    const meta = { name, action, schema: this.definedSchema[name] };
+
+    // this will call the hooks with the situation, allowing it to change the args or result
+    // based on the stage
+    const callHooks = (stage, changeable) => {
+      return _.reduce(hooks, (inbound, hook) => {
+        const result = hook({ ...meta, stage, ...inbound });
+        return result ? result : inbound;
+      }, changeable);
+    };
+
+    // It goes logically :) - BEFORE hooks, call, AFTER hooks
+    return new Promise((resolve, reject) => {
+      try {
+        const resolvedArgs = callHooks(BEFORE, args);
+
+        logic(this.model(name), resolvedArgs)
+          .then((result) => {
+            const resolvedResponse = callHooks(AFTER, { result });
+
+            resolve(resolvedResponse.result);
+          })
+          .catch(reject);
+      } catch (err) {
+        console.log(err);
+        reject(err);
+      }
+    });
+  }
+
   findById(name, id) {
-    return this.model(name).findById(id);
+    const logic = (model, args) => model.findById(args.id)
+      .then(result => result.length === 0 ? null : result[0].dataValues);
+
+    return this.execute(name, QUERY, { id }, logic);
   }
 
   find(name, query) {
-    return this.model(name).findAll({ where: query });
+    const logic = (model, args) => model.findAll({ where: args.query })
+      .then(result => result.map(item => item.dataValues));
+
+    return this.execute(name, QUERY, { query }, logic);
   }
 
   create(name, data) {
-    return this.model(name).create(data, { returning: true });
+    const logic = (model, args) => model.create(args.data, { returning: true });
+
+    return this.execute(name, INSERT, { data }, logic);
   }
 
   update(name, id, data) {
-    return this.model(name).update(data, { where: { id } });
+    const logic = (model, args) => model.update(args.data, { where: { id: args.id } });
+
+    return this.execute(name, UPDATE, { id, data }, logic);
   }
 
   delete(name, id) {
-    return this.model(name).destroy({ where: { id } });
+    const logic = (model, args) => model.destroy({ where: { id: args.id } });
+
+    return this.execute(name, DELETE, { id }, logic);
   }
 }
