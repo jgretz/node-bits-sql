af2ae12352098b6673fadbbb07f6eb569c5edfb5
jgretz <jgretz@truefitsolutions.com>
2017-03-14 13:29:37 -0400

    add pagination

--- src/util/options/page.js
+++ src/util/options/page.js
@@ -1,3 +1,13 @@
 export const buildPage = args => {
-  return {};
+  const page = {};
+
+  if (args.start) {
+    page.offset = args.start;
+  }
+
+  if (args.max) {
+    page.limit = args.max;
+  }
+
+  return page;
 };
