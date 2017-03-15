f5e01c8cca7bd6f480c78cc83bac421d1a97440e
jgretz <jgretz@truefitsolutions.com>
2017-03-15 07:16:53 -0400

    fix call without args for build options

--- src/util/build_options.js
+++ src/util/build_options.js
@@ -1,7 +1,7 @@
 import {buildInclude, buildWhere, buildPage, buildOrderby, buildSelect} from './options';
 
 
-export const buildOptions = (mode, model, db, models, args) => {
+export const buildOptions = (mode, model, db, models, args = {}) => {
   const include = buildInclude(mode, model, db, models, [], args.select || []);
   const where = buildWhere(args);
   const page = buildPage(args);
